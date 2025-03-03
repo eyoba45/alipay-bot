388877b2fcc556777d4559c066ef0514192117d5
eyoba45 <164791538+eyoba45@users.noreply.github.com>
2025-03-03 22:38:24 +0300

    Update models.py

--- models.py
+++ models.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 from sqlalchemy import Column, Integer, String, Float, DateTime, BigInteger, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import declarative_base, relationship
 
 Base = declarative_base()
 
